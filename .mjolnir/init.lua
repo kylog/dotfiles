6a59dcd01061016e86484a00513009ae8ece4764
Kylo Ginsberg <kylo@puppetlabs.com>
2015-01-30 20:36:50 -0800

    Initial commit

---
+++ .mjolnir/init.lua
@@ -0,0 +1,158 @@
+-- Load Extensions
+local application = require "mjolnir.application"
+local window = require "mjolnir.window"
+local hotkey = require "mjolnir.hotkey"
+local keycodes = require "mjolnir.keycodes"
+local fnutils = require "mjolnir.fnutils"
+local alert = require "mjolnir.alert"
+local screen = require "mjolnir.screen"
+-- User packages
+local grid = require "mjolnir.bg.grid"
+local hints = require "mjolnir.th.hints"
+local appfinder = require "mjolnir.cmsj.appfinder"
+
+local definitions = nil
+local hyper = nil
+
+local gridset = function(frame)
+  return function()
+    local win = window.focusedwindow()
+    if win then
+      grid.set(win, frame, win:screen())
+    else
+      alert.show("No focused window.")
+    end
+  end
+end
+
+auxWin = nil
+function saveFocus()
+  auxWin = window.focusedwindow()
+  alert.show("Window '" .. auxWin:title() .. "' saved.")
+end
+function focusSaved()
+  if auxWin then
+    auxWin:focus()
+  end
+end
+
+local hotkeys = {}
+
+function createHotkeys()
+  for key, fun in pairs(definitions) do
+    local mod = hyper
+    if string.len(key) == 2 and string.sub(key,2,2) == "c" then
+      mod = {"cmd"}
+    end
+
+    local hk = hotkey.new(mod, string.sub(key,1,1), fun)
+    table.insert(hotkeys, hk)
+    hk:enable()
+  end
+end
+
+function rebindHotkeys()
+  for i, hk in ipairs(hotkeys) do
+    hk:disable()
+  end
+  hotkeys = {}
+  createHotkeys()
+  alert.show("Rebound Hotkeys")
+end
+
+function applyPlace(win, place)
+  local scrs = screen:allscreens()
+  local scr = scrs[place[1]]
+  grid.set(win, place[2], scr)
+end
+
+function applyLayout(layout)
+  return function()
+    for appName, place in pairs(layout) do
+      local app = appfinder.app_from_name(appName)
+      if app then
+        for i, win in ipairs(app:allwindows()) do
+          applyPlace(win, place)
+        end
+      end
+    end
+  end
+end
+
+function init()
+  createHotkeys()
+  keycodes.inputsourcechanged(rebindHotkeys)
+  alert.show("Mjolnir, at your service.")
+end
+
+-- Actual config =================================
+
+hyper = {"cmd", "alt", "ctrl","shift"}
+-- Set grid size.
+grid.GRIDWIDTH  = 6
+grid.GRIDHEIGHT = 8
+grid.MARGINX = 0
+grid.MARGINY = 0
+local gw = grid.GRIDWIDTH
+local gh = grid.GRIDHEIGHT
+
+local gomiddle = {x = 1, y = 1, w = 4, h = 6}
+local goleft = {x = 0, y = 0, w = gw/2, h = gh}
+local goright = {x = gw/2, y = 0, w = gw/2, h = gh}
+local gobig = {x = 0, y = 0, w = gw, h = gh}
+
+local fullApps = {
+  "Safari","Aurora","Nightly","Xcode","Qt Creator","Google Chrome",
+  "Google Chrome Canary", "Eclipse", "Coda 2", "iTunes", "Emacs", "Firefox"
+}
+local layout2 = {
+  Airmail = {1, gomiddle},
+  Spotify = {1, gomiddle},
+  Calendar = {1, gomiddle},
+  Dash = {1, gomiddle},
+  iTerm = {2, goright},
+  MacRanger = {2, goleft},
+}
+fnutils.each(fullApps, function(app) layout2[app] = {1, gobig} end)
+
+definitions = {
+  [";"] = saveFocus,
+  a = focusSaved,
+
+  h = gridset(gomiddle),
+  t = gridset(goleft),
+  n = grid.maximize_window,
+  s = gridset(goright),
+
+  g = applyLayout(layout2),
+
+  d = grid.pushwindow_nextscreen,
+  r = mjolnir.reload,
+  q = function() appfinder.app_from_name("Mjolnir"):kill() end,
+
+  k = function() hints.appHints(appfinder.app_from_name("Emacs")) end,
+  j = function() hints.appHints(window.focusedwindow():application()) end,
+  ec = hints.windowHints
+}
+
+-- launch and focus applications
+fnutils.each({
+  { key = "o", app = "MacRanger" },
+  { key = "e", app = "Google Chrome" },
+  { key = "u", app = "Emacs" },
+  { key = "i", app = "iTerm" },
+  { key = "m", app = "Airmail" }
+}, function(object)
+    definitions[object.key] = function() application.launchorfocus(object.app) end
+end)
+
+init()
+
+hotkey.bind({"cmd", "alt", "ctrl"}, "D", function()
+  local win = window.focusedwindow()
+  local f = win:frame()
+  f.x = f.x + 10
+  win:setframe(f)
+end)
+
+
